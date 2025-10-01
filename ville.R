3aa763c011ea7ab0bc94edd352e2dbd719530a48
Ville-Veikko Patjas <ville.patjas@gmail.com>
2025-10-01 15:52:39 +0300

    jee jee

---
+++ ville.R
@@ -0,0 +1,20 @@
+source("./GIT_DEMO.R")
+source("./ENV.R")
+
+con <- connect_to_snowflake()
+
+data <- dbGetQuery(con, "SELECT * FROM SANDBOX.RISK.GIT_DEMO")
+
+print(data)
+
+Name <- "ville"
+Message <- "Git gud"
+Rating <- 10
+
+insert_into_git_demo(con, Message, Name, Rating)
+
+data <- dbGetQuery(con, "SELECT * FROM SANDBOX.RISK.GIT_DEMO")
+
+print(data)
+
+dbDisconnect(con)
