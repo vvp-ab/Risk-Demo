8258d8d2e58c7682c15dd511d6273536365abfa8
sasotti <samuele.sottile@alandsbanken.se>
2025-10-09 14:11:29 +0200

    Change

--- Samuele.R
+++ Samuele.R
@@ -1,11 +1,24 @@
-# Calculate the inverse CDF (quantile) for the standard normal distribution
-p <- 0.999
-R <- 0.15
-part1 <- -1/(sqrt(R))*qnorm(p) 
+source("./GIT_DEMO.R")
+source("./ENV.R")
 
-part2 <- + sqrt((1-R)/R)*sqrt( (qnorm(p))^2 -log(1-R)) 
+con <- connect_to_snowflake()
 
-result <- pnorm(part1 + part2)
+# Query for inital state of the table containing demo reviews
+data <- dbGetQuery(con, "SELECT * FROM SANDBOX.RISK.GIT_DEMO")
 
-# Print the result
-print(result)
+print(data)
+
+# Name of reviewer, their feedback about the demo and rating (1-10)
+Name <- "Ålands Banker"
+Message <- "Insert a review here."
+Rating <- 5
+
+# Updates SANDBOX.RISK.GIT_DEMO table with the new review
+insert_into_git_demo(con, Message, Name, Rating)
+
+# Query for updated state of the table containing demo reviews
+data <- dbGetQuery(con, "SELECT * FROM SANDBOX.RISK.GIT_DEMO")
+
+print(data)
+
+dbDisconnect(con)
